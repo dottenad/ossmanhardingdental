cb5a4c89fbc90f35244b56db16578a827127f65c
dottenad <ottenadd@gmail.com>
2026-04-13 14:52:47 -0700

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -79,6 +79,18 @@ export default function RootLayout({
                         `,
                     }}
                 />
+                {/* Microsoft Clarity */}
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            (function(c,l,a,r,i,t,y){
+                                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+                                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                            })(window, document, "clarity", "script", "wb8zcnna11");
+                        `,
+                    }}
+                />
             </head>
             <body className={`${(font as any).variable || ""} ${(headingFont as any)?.variable || ""} font-sans`}>
                 <Analytics />
